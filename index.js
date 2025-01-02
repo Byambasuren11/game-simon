3ccb13afe1518cd0076d91e3376d4875fc5b3cbe
Byambasuren11 <byumbasurenbold@gmail.com>
2025-01-02 14:30:43 +0800

    hello

--- index.js
+++ index.js
@@ -1,140 +1,25 @@
-// Simon Game Implementation in JavaScript
-// Initialize variables
-let sequence = [];
-let playerSequence = [];
-let level = 0;
+const main = document.createElement("div");
+main.className = "main";
 
-const colors = ["leftTop", "rightTop", "leftBottom", "rightBottom"];
+const homePageImg = document.createElement("img");
+homePageImg.src = "homePage.png";
 
-const sounds = {
-  leftTop: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-  rightTop: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-  leftBottom: new Audio(
-    "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"
-  ),
-  rightBottom: new Audio(
-    "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
-  ),
-};
+const welcomeText = document.createElement("div");
+welcomeText.innerHTML = "Welcome to Simon Says!";
+welcomeText.id = "welcomeText";
 
-// Start the game
-function startGame() {
-  sequence = [];
-  playerSequence = [];
-  level = 0;
-  nextLevel();
-}
+const playButtonForKids = document.createElement("a");
+playButtonForKids.innerHTML = "Play For kids!";
+playButtonForKids.id = "playButtonForKids";
+playButtonForKids.href = "./simon.html";
 
-// Generate the next sequence
-function nextLevel() {
-  level++;
-  level1.innerHTML = level;
-  playerSequence = [];
-  const nextColor = colors[Math.floor(Math.random() * colors.length)];
-  sequence.push(nextColor);
-  playSequence();
-}
+const playButtonStandart = document.createElement("a");
+playButtonStandart.innerHTML = "Play Standart";
+playButtonStandart.id = "playButtonStandart";
+playButtonStandart.href = "./index.html";
 
-function playSound(color) {
-  if (sounds[color]) {
-    sounds[color].currentTime = 0; // Reset playback for consecutive presses
-    sounds[color].play();
-  }
-}
-
-// Play the current sequence
-function playSequence() {
-  sequence.forEach((color, index) => {
-    setTimeout(() => {
-      flashColor(color);
-    }, (index + 1) * 1000);
-  });
-}
-
-// Flash the color on the screen
-function flashColor(color) {
-  const button = document.getElementById(color);
-  playSound(color);
-  button.style.animation = `ajillah${colors.indexOf(color) + 1} 0.5s linear`;
-  setTimeout(() => {
-    button.style.animation = "none";
-  }, 500);
-}
-
-// Handle player input
-function handlePlayerInput(color) {
-  playerSequence.push(color);
-  flashColor(color);
-  checkPlayerInput();
-}
-
-// Check if the player's input is correct
-function checkPlayerInput() {
-  const currentIndex = playerSequence.length - 1;
-
-  if (playerSequence[currentIndex] !== sequence[currentIndex]) {
-    gameOver.style.display = "block";
-    gameOverCancel.style.display = "block";
-    startGame();
-    return;
-  }
-
-  if (playerSequence.length === sequence.length) {
-    setTimeout(nextLevel, 500);
-  }
-}
-
-// UI Creation Code
-let rightTop = document.createElement("button");
-rightTop.id = "rightTop";
-let leftTop = document.createElement("button");
-leftTop.id = "leftTop";
-let rightBottom = document.createElement("button");
-rightBottom.id = "rightBottom";
-let leftBottom = document.createElement("button");
-leftBottom.id = "leftBottom";
-
-const gameOver = document.createElement("div");
-gameOver.className = "gameOver";
-gameOver.textContent = "Game Over!";
-document.getElementById("mainContain").appendChild(gameOver);
-
-const gameOverCancel = document.createElement("img");
-gameOverCancel.src = "closeicon.png";
-gameOverCancel.className = "close";
-document.getElementById("mainContain").appendChild(gameOverCancel);
-
-[rightTop, leftTop, rightBottom, leftBottom].forEach((button) => {
-  button.className = "color-button";
-  document.getElementById("mainContain").appendChild(button);
-});
-
-let midCircle = document.createElement("div");
-midCircle.id = "midCircle";
-
-let midCircleTitle = document.createElement("h2");
-midCircleTitle.id = "midCircle_garchig";
-midCircleTitle.innerHTML = "Simon";
-
-let level1 = document.createElement("div");
-level1.className = "level";
-level1.innerHTML = "0";
-
-let startButton = document.createElement("button");
-startButton.id = "midCircle_dund";
-startButton.innerHTML = "Start";
-midCircle.append(midCircleTitle, level1, startButton);
-document.getElementById("mainContain").appendChild(midCircle);
-
-gameOverCancel.addEventListener("click", () => {
-  gameOver.style.display = "none";
-  gameOverCancel.style.display = "none";
-});
-
-// Set up event listeners
-startButton.addEventListener("click", startGame);
-document.querySelectorAll(".color-button").forEach((button) => {
-  button.addEventListener("click", () => {
-    handlePlayerInput(button.id);
-  });
-});
+main.appendChild(playButtonStandart);
+main.appendChild(playButtonForKids);
+main.appendChild(welcomeText);
+main.appendChild(homePageImg);
+document.getElementById("landingPage").appendChild(main);
